ad099cc3be0887f361abf9009554d0d278076c83
Stella <Stella@DESKTOP-NREV4QF>
2018-12-15 11:17:17 +0700

    versi 8

--- RPL_WebService_AntrianRS/src/java/service/rumahSakitResource.java
+++ RPL_WebService_AntrianRS/src/java/service/rumahSakitResource.java
@@ -14,7 +14,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -66,7 +65,6 @@ public class rumahSakitResource {
     /**
      * PUT method for updating or creating an instance of rumahSakitResource
      * @param data
-     * @param content representation for the resource
      * @return 
      */
 //    @PUT
